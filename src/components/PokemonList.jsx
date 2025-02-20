3d84f4ea644499c2b8b68d724e8f6f3cce47e332
muskan <muskanloach984@gmail.com>
2025-02-20 22:23:02 +0530

    fix

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,14 +1,28 @@
-
+import { useState, useEffect } from "react";
 import PokemonCard from "./Pokemon";
 import PropTypes from "prop-types";
 
-
 const PokemonList = ({ pokemons }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (pokemons.length > 0) {
+      // Simulate a delay for loading effect
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000); // Adjust time as needed
+    }
+  }, [pokemons]);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {pokemons.map((pokemon) => (
-        <PokemonCard key={pokemon.id} pokemon={pokemon} />
-      ))}
+      {loading ? (
+        <p className="text-center col-span-full text-gray-500">Loading Pokémons...</p>
+      ) : (
+        pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))
+      )}
     </div>
   );
 };
